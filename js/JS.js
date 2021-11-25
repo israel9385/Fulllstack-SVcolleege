a76e6d41871052913fe3ce79c10b6d907f072739
israel9385 <54075314+israel9385@users.noreply.github.com>
2021-11-25 16:32:47 +0200

    Add files via upload

---
+++ js/JS.js
@@ -0,0 +1,80 @@
+// #1
+// let int1 = 10;
+// let int2 = 20;
+// let int3 = 30;
+
+// let sum = int1 * int2 * int3;
+// console.log(sum);
+
+// #2
+
+// let fn = "israel";
+// let ln = "kadosh";
+// let bw = fn;
+// fn = ln;
+// console.log(bw + ' ' + fn);
+
+// #3
+
+// let fullName = ("israel Kadosh");
+// debugger;
+// let space = fullName.indexOf(" ");
+// fullName = fullName.splice(0, space);
+// console.log(fullName);
+
+// #4
+// let int1 = 10;
+// let int2 = 20;
+// let sum = int1 ** int2;
+// console.log(sum);
+
+// let letter='A';
+// let str='BBBBB';
+// let middle=Math.round(str.length/2);
+// let str1=str.slice(-1,0)+letter+(str.slice(2,3))+letter+(str.slice(3)+letter);
+// console.log(str1);
+
+// #5
+// let str='Israel';
+// let letter='A';
+// let middle=Math.round(str.length/2);
+// let newStr=letter+str.slice(0,middle)+letter+str.slice(middle)+letter;
+// console.log(newStr);
+
+
+
+// #6
+// let num1 = 10;
+// let num2 = 11;
+// let num3 = 12;
+// let num4 = 13;
+// let num5 = 14;
+
+// let sum = (num1 * num2 * num3 * num4 * num5);
+// console.log(sum);
+// console.log(num1%10);
+// console.log(num2%10);
+// console.log(num3%10);
+// console.log(num4%10);
+// console.log(num5%10);
+
+//  #7
+// let strA='Israel';
+// let strB='Kadosh';
+// let newStr=strA+' '+strB;
+// console.log(newStr);
+
+// #8
+// let str='Israel Kadosh';
+// let index1 = str.indexOf('Israel');
+// let index2 = str.indexOf('Kadosh');
+// console.log(index1 +' ' + index2);
+
+//  #9
+// let num1=85;
+// let num2=20;
+// debugger;
+// num2+=num1;
+// num1=num2-num1;
+// num2-=num1;
+// console.log('num1:',num1,',num2:',num2);
